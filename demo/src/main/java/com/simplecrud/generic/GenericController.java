4b51fc7d9b14890471a8b150459ff1b99b8ef79b
Christopher Iliyas <christopheriliyas@gmail.com>
2020-02-20 16:30:17 +0100

    add custom exception handler

--- demo/src/main/java/com/simplecrud/generic/GenericController.java
+++ demo/src/main/java/com/simplecrud/generic/GenericController.java
@@ -1,21 +1,21 @@
-package com.simplecrud.generic;
+// package com.simplecrud.generic;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.boot.web.servlet.error.ErrorController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GenericController implements ErrorController{
+// @RestController
+// public class GenericController implements ErrorController{
 
-    private static final String PATH = "/error";
+//     private static final String PATH = "/error";
 
-    @RequestMapping(value = PATH)
-    public String error() {
-        return "this path does not exist";
-    }
+//     @RequestMapping(value = PATH)
+//     public String error() {
+//         return "this path does not exist";
+//     }
 
-    @Override
-    public String getErrorPath() {
-        return PATH;
-    }
-}
+//     @Override
+//     public String getErrorPath() {
+//         return PATH;
+//     }
+// }
--- demo/src/main/java/com/simplecrud/user/UserController.java
+++ demo/src/main/java/com/simplecrud/user/UserController.java
@@ -1,11 +1,17 @@
 package com.simplecrud.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class UserController {
@@ -20,6 +26,20 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public User getOneUser(@PathVariable int id) {
-        return userService.findOne(id);
+        User user = userService.findOne(id);
+
+        if(user == null) {
+            throw new UserNotFoundException("id -" + id);
+        }
+        return user;
     }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+    
 }
